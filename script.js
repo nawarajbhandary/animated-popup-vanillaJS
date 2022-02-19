53db87932246b590f031469f9b9f0ab3e6a8e9fe
Nawaraj Bhandari <nawarajbhandary03@gmail.com>
2022-02-19 19:35:44 +0545

    Send Mail functionality added to project

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ OUR JS part is all about implemention of classList
 
 const section = document.querySelector("section"),
   hireBtn = section.querySelector("#hireBtn"),
-  closeBtn = section.querySelectorAll("#close");
+  closeBtn = section.querySelectorAll("#close"),
+  mailBtn = section.querySelector("#send"),
+  mailBody = section.querySelector("#textArea").value;
 
 hireBtn.addEventListener("click", () => {
   section.classList.add("show");
@@ -14,4 +16,8 @@ closeBtn.forEach((cBtn) => {
   cBtn.addEventListener("click", () => {
     section.classList.remove("show");
   });
+});
+
+mailBtn.addEventListener("click", () => {
+  window.location.assign("mailto:nawarajbhandary03@gmail.com?body" + mailBody);
 });
